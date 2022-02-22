5d74f8f5100ddb1f135327fa95c9e61efc0a0ec7
VincentArellano <vincentvarellano08@gmail.com>
2022-02-22 11:13:18 +0000

    connect databse

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=shop-images';
+    $dsn = 'mysql:host=localhost;dbname=ca2_crud_shop';
     $username = 'root';
-    $password = 'root';
+    $password = '';
 
     try {
         $db = new PDO($dsn, $username, $password);
